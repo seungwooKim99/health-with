2e57c19a42af2accb7eece2c4ec104ee088a4abb
nowkim <peterhyunjae@naver.com>
2021-08-19 15:14:16 +0900

    font 적용 시 테스트 코드 삭제

--- constants/fonts.js
+++
@@ -1,9 +0,0 @@
-export const robotoBlack = require("../assets/fonts/Roboto-Black.ttf");
-export const robotoBold = require("../assets/fonts/Roboto-Bold.ttf");
-export const robotoRegular = require("../assets/fonts/Roboto-Regular.ttf");
-
-export default {
-    robotoBlack,
-    robotoBold,
-    robotoRegular
-}
--- constants/theme.js
+++ constants/theme.js
@@ -1,6 +1,5 @@
 import { Dimensions } from "react-native";
 const { width, height } = Dimensions.get("window");
-import fonts from "./fonts";
 
 import { useFonts } from 'expo-font';
 
@@ -51,44 +50,6 @@ export const SIZES = {
   height,
 };
 
-export const FONTS = {
-  
-  largeTitle: {
-    fontFamily: fonts.robotoRegular,
-    fontSize: SIZES.largeTitle,
-    lineHeight: 55,
-  },
-  h1: { fontFamily: "Roboto-Black", fontSize: SIZES.h1, lineHeight: 36 },
-  h2: { fontFamily: fonts.robotoBold, fontSize: SIZES.h2, lineHeight: 30 },
-  h3: { fontFamily: "Roboto-Bold", fontSize: SIZES.h3, lineHeight: 22 },
-  h4: { fontFamily: "Roboto-Bold", fontSize: SIZES.h4, lineHeight: 22 },
-  body1: {
-    fontFamily: "Roboto-Regular",
-    fontSize: SIZES.body1,
-    lineHeight: 36,
-  },
-  body2: {
-    fontFamily: "Roboto-Regular",
-    fontSize: SIZES.body2,
-    lineHeight: 30,
-  },
-  body3: {
-    fontFamily: "Roboto-Regular",
-    fontSize: SIZES.body3,
-    lineHeight: 22,
-  },
-  body4: {
-    fontFamily: "Roboto-Regular",
-    fontSize: SIZES.body4,
-    lineHeight: 22,
-  },
-  body5: {
-    fontFamily: "Roboto-Regular",
-    fontSize: SIZES.body5,
-    lineHeight: 22,
-  },
-};
-
-const appTheme = { COLORS, SIZES, FONTS };
+const appTheme = { COLORS, SIZES };
 
 export default appTheme;
